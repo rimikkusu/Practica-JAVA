48783f1877e41f81e18daa818ec8167d3cc56973
Bragari Eduard <ebragari@gmail.com>
2026-05-06 09:00:29 +0300

    Third commit

---
+++ auto_inchiriere/src/main/java/com/bragari/models/CategorieAuto.java
@@ -0,0 +1,12 @@
+package com.bragari.models;
+
+public class CategorieAuto extends BaseEntity {
+    private String denumire;
+    private String descriere;
+    
+    public CategorieAuto() {
+    }
+
+    public 
+    
+}
